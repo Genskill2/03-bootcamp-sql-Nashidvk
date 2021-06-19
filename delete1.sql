e8620972b2c2ae2d1994a5e0011ce3545c4bccd7
Nashidvk <muhammadnaseemnashid@gmail.com>
2021-06-19 13:26:11 +0530

    Added Delete all Science Fiction sequel file

---
+++ delete1.sql
@@ -0,0 +1,4 @@
+-- Write a file called delete1.sql that contains a SQL statement to delete the subject 'History'.
+-- The subject must be removed from the tables and from all the books. You will need two queries for this. First to delete all the history mappings in the books_subjects table. Then to delete the subject itsel
+delete from books_subjects where books_subjects.subject = ( select id from subjects where name = "History");
+delete from subjects where name = "History";
